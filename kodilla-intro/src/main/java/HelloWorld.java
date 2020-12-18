e65285d5bc21a84ee4caa23055440a56698eb3d6
Justyna Szczygiol <justynaszczygiol@gmail.com>
2020-12-18 21:08:47 +0100

    0.7 HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Justyna");
+        System.out.println(27);
+        System.out.println('S');
+    }
+}
