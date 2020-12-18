24cd38e530bef160bbdca7b0c63427ade846a814
Justyna Szczygiol <justynaszczygiol@gmail.com>
2020-12-18 21:31:35 +0100

    0.8 MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Cola";
+        String typeOfAnimal = "dog";
+        int age = 3;
+        double weight = 20.5;
+        boolean isItBrown = true;
+        char boyOrGirl = 'G';
+
+        System.out.println(name);
+        System.out.println(typeOfAnimal);
+        System.out.println(age);
+        System.out.println(weight);
+        System.out.println(isItBrown);
+        System.out.println(boyOrGirl);
+    }
+}
