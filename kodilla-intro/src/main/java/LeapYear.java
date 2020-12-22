a5d085793b168c34ed039ca77a1e11f15a6a3dd1
Justyna Szczygiol <justynaszczygiol@gmail.com>
2020-12-22 21:17:08 +0100

    0.11 LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,22 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2000;
+        String test = isItLeapYear(year);
+        System.out.println(test);
+    }
+
+    private static String isItLeapYear(int a) {
+        String leapYear = "This is leap year.";
+        String notLeapYear = "This is not leap year.";
+
+        if (a % 4 != 0) {
+            return notLeapYear;
+        } else if (a % 100 != 0) {
+            return leapYear;
+        } else if (a % 400 != 0) {
+            return notLeapYear;
+        } else {
+            return leapYear;
+            }
+        }
+}
