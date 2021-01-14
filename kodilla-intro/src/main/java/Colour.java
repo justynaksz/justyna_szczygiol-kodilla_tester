96cc30011dfe17ac0368f9024f6e36ea981a57e6
Justyna Szczygiol <justynaszczygiol@gmail.com>
2021-01-14 21:31:40 +0100

    2.8 Colour

---
+++ kodilla-intro/src/main/java/Colour.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class Colour {
+    public static String colour() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter first letter of the colour.");
+            String letter = scanner.nextLine().trim().toLowerCase();
+            switch (letter) {
+                case "w":
+                    return "white";
+                case "b":
+                    return "black";
+                case "g":
+                    return "green";
+                case "y":
+                    return "yellow";
+                case "p":
+                    return "purple";
+                default:
+                    System.out.println("Select different letter.");
+            }
+        }
+    }
+}
