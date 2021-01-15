135d8d928fd42e77daa0e46a20148f69b6614d3b
Justyna Szczygiol <justynaszczygiol@gmail.com>
2021-01-15 09:43:28 +0100

    2.4 Grades

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,29 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastGrades() {
+        return this.grades[this.size - 1];
+    }
+
+    public double gradesAverage() {
+        double sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = sum + this.grades[i];
+        }
+        return sum / this.size;
+    }
+}
