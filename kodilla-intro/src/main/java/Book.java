0ee073f75c7da3fbef4aae07a6b750d8401499af
Justyna Szczygiol <justynaszczygiol@gmail.com>
2021-01-17 21:29:43 +0100

    2.7 Book

---
+++ kodilla-intro/src/main/java/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private String author;
+    private String title;
+
+    public static Book of(String author, String title) {
+        Book book = new Book();
+        book.author = author;
+        book.title = title;
+        return book;
+    }
+}
