9b10df5e65a52d17466270817733d269c819dee8
Justyna Szczygiol <justynaszczygiol@gmail.com>
2021-01-21 21:04:37 +0100

    3.6 CalculatorTestSuite

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -1,2 +1,49 @@
-package com.kodilla.basic_assertion;public class CalculatorTestSuite {
+package com.kodilla.basic_assertion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTestSuite {
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        assertEquals(13, sumResult);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int subtractResult = calculator.subtract(a, b);
+        assertEquals(-3, subtractResult);
+    }
+
+    @Test
+    public void testSqrtNegativeNumber() {
+        Calculator calculator = new Calculator();
+        int a = -3;
+        int sqrtResult = calculator.sqrt(a);
+        assertEquals(9, sqrtResult);
+    }
+
+    @Test
+    public void testSqrtZero() {
+        Calculator calculator = new Calculator();
+        int a = 0;
+        int sqrResult = calculator.sqrt(a);
+        assertEquals(0,sqrResult);
+    }
+
+    @Test
+    public void testSqrtPositive() {
+        Calculator calculator = new Calculator();
+        int a = 4;
+        int sqrResult = calculator.sqrt(a);
+        assertEquals(16,sqrResult);
+    }
 }
