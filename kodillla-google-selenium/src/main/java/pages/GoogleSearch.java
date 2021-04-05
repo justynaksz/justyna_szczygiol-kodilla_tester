44a869acf07de8360181e2bd743972a80ed5abea
Justyna Szczygiol <justynaszczygiol@gmail.com>
2021-04-05 21:49:32 +0200

    "13.6 Google 2.0"

--- kodillla-google-selenium/src/main/java/pages/GoogleSearch.java
+++ kodillla-google-selenium/src/main/java/pages/GoogleSearch.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +9,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
-public class GoogleSearch {
+public class GoogleSearch extends AbstractPage {
 
     @FindBy(xpath = "//html/body/div[2]/div[2]/div[3]/span/div/div/div[3]/button[2]")
     static WebElement acceptCookies;
@@ -21,32 +20,25 @@ public class GoogleSearch {
     @FindBy(css = "input[value='Szukaj w Google']")
     static WebElement searchButton;
 
-    //u mnie były polskie nazwy, "Szukaj w Google" było w HTMLu tylko raz, dlatego nie ma listy
+    private GoogleResults googleResults;
 
-    private static GoogleResults googleResults;
+    public GoogleSearch(WebDriver driver) {
+        super(driver);
+    }
 
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\IdeaProjects\\kodilla-course\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        driver.navigate().to("http://www.google.com");
-
+    public void searchResults() {
         PageFactory.initElements(driver, GoogleSearch.class);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//html/body/div[2]/div[2]/div[3]/span/div/div/div[3]/button[2]")));
         acceptCookies.click();
         inputField.sendKeys("Kodilla");
         googleResults = loadResults(driver);
         googleResults.iSeeResults();
-        driver.close();
     }
 
-   public static GoogleResults loadResults(WebDriver driver) {
+
+    public GoogleResults loadResults(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(searchButton)).click();
-
-        // nie działa kliknięcie w opcję szukania
-
-        GoogleResults googleResults = new GoogleResults();
+        GoogleResults googleResults = new GoogleResults(driver);
         return googleResults;
     }
 }
